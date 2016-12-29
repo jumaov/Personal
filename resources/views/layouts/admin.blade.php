2850884594febc938c958f5ed4b9f45fd4827159
Juan Manuel Oviedo Carrasco <juanmanuel85@gmail.com>
2016-12-29 04:19:53 -0400

    Organizacion de Menú Principal
    Agregue las rutas con {{url('aqui la ruta'}}

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -87,11 +87,11 @@
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-laptop"></i>
-                <span>Almacén</span>
+                <span>Personal</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="almacen/articulo"><i class="fa fa-circle-o"></i> Artículos</a></li>
+                <li><a href="{{url('personal/persona')}}"><i class="fa fa-circle-o"></i> Personas</a></li>
                 <li><a href="almacen/categoria"><i class="fa fa-circle-o"></i> Categorías</a></li>
               </ul>
             </li>
@@ -121,11 +121,11 @@
                        
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-folder"></i> <span>Acceso Personal</span>
+                <i class="fa fa-folder"></i> <span>Acceso</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="configuracion/usuario"><i class="fa fa-circle-o"></i> Usuarios</a></li>
+                <li><a href="{{url('seguridad/usuario')}}"><i class="fa fa-circle-o"></i> Usuarios</a></li>
                 
               </ul>
             </li>
